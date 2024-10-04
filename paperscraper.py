b8f40a0b7312ea6ca5f51adc60a3b311d3dfe757
Josh Clark <joshajc99@live.co.uk>
2024-10-05 00:27:42 +0100

    Upgraded get links to retrieve the pdf url instead, as pdf urls follow same format to paper's webpage url anyway

--- paperscraper.py
+++ paperscraper.py
@@ -8,9 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-# Define base URL for HuggingFace
-BASE_URL = "https://huggingface.co"
-
 # Define class
 class paperscraper:
 
@@ -19,46 +16,29 @@ class paperscraper:
         self.date = date
 
         # Define base URL for HuggingFace
-        self.BASE_URL = "https://huggingface.co"
+        self.HF_BASE_URL = "https://huggingface.co"
+        self.PDF_BASE_URL = "https://arxiv.org/pdf/"
 
-    # Define function to get links to all paper webpages
+    # Define function to get links to all paper pdfs
     def get_links(self):
         # Define URL for daily papers for given date
-        URL = f"{self.BASE_URL}/papers?date={self.date}"
+        URL = f"{self.HF_BASE_URL}/papers?date={self.date}"
 
         # Parse the html from the daily papers page
         response = requests.get(URL)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Initialise dictionary to store links into
-        paper_pages = {}
+        paper_pdfs = {}
 
         # Extract links to individual papers using huggingface html structure
         paper_links = soup.find_all('a', class_='line-clamp-3 cursor-pointer text-balance')
 
         # Loop through papers to store paper_title: url pairs
         for paper in paper_links:
-            paper_title = paper.text.strip()        # Get the title of the paper
-            paper_page_url = paper['href']          # Get the relative link to the paper's Hugging Face page
-            paper_pages[paper_title] = f"{BASE_URL}{paper_page_url}"    # Store the title: url pair into dict
+            paper_title = paper.text.strip()                                                    # Get the title of the paper
+            paper_page_url = paper['href']                                                      # Get the relative link to the paper's Hugging Face page
+            paper_pdfs[paper_title] = f"{self.PDF_BASE_URL}{paper_page_url.strip('/papers/')}"  # Store the title: url pair into dict
 
-        return paper_pages
-
-    
-    # Define function to get the link to each paper's pdf
-    def get_paper_text(self, paper_pages):
-        # Initialise dictionary to store links into
-        paper_pdfs = {}
-
-        # Loop through each paper in paper_pages dict
-        for paper, page in paper_pages.items():
-            # Parse the html from the paper's page
-            response = requests.get(page)
-            soup = BeautifulSoup(response.content, 'html.parser')
-
-            # Extract link to pdf
-            pdf_link = soup.find_all('a', class_='btn inline-flex h-9 items-center')
-
-            # Store pdf link
-            paper_pdfs[paper] = pdf_link
+        return paper_pdfs
 
