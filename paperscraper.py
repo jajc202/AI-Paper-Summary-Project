c707b502f83529ca68128eb5259dec39099b325d
Josh Clark <joshajc99@live.co.uk>
2024-10-15 20:05:28 +0100

    Added function to get link to html source for the paper instead of pdf. This is because html source will be easier to extract structure from

--- paperscraper.py
+++ paperscraper.py
@@ -19,9 +19,9 @@ class paperscraper:
 
         # Define base URL for HuggingFace
         self.HF_BASE_URL = "https://huggingface.co"
-        self.PDF_BASE_URL = "https://arxiv.org/pdf/"
+        self.ARXIV_BASE_URL = "https://arxiv.org/abs/"
 
-    # Define function to get links to all paper pdfs
+    # Define function to get links to arxiv page for all papers
     def get_links(self):
         # Define URL for daily papers for given date
         URL = f"{self.HF_BASE_URL}/papers?date={self.date}"
@@ -31,7 +31,7 @@ class paperscraper:
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Initialise dictionary to store links into
-        paper_pdfs = {}
+        paper_pages = {}
 
         # Extract links to individual papers using huggingface html structure
         paper_links = soup.find_all('a', class_='line-clamp-3 cursor-pointer text-balance')
@@ -40,42 +40,42 @@ class paperscraper:
         for paper in paper_links:
             paper_title = paper.text.strip()                                                    # Get the title of the paper
             paper_page_url = paper['href']                                                      # Get the relative link to the paper's Hugging Face page
-            paper_pdfs[paper_title] = f"{self.PDF_BASE_URL}{paper_page_url.strip('/papers/')}"  # Store the title: url pair into dict
+            paper_pages[paper_title] = f"{self.ARXIV_BASE_URL}{paper_page_url.strip('/papers/')}"  # Store the title: url pair into dict
 
-        return paper_pdfs
+        return paper_pages
     
-
-    # Define function to extract text from pdf link
-    def get_pdf_text(self, paper_pdfs):
-        # Initialise dictionary to store plain text into
-        pdf_texts = {}
+    
+    # Define function to extract html link from arxiv link
+    def get_html_link(self, paper_pages):
+        # Initialise dictionary to store html into
+        html_links = {}
 
         # Loop through each paper
-        for paper, url in paper_pdfs.items():
+        for paper, url in paper_pages.items():
             try:
                 # Send a get request to the pdf url
                 response = requests.get(url)
 
                 # Check if the request was successful (status code 200)
                 if response.status_code == 200:
-                    # Read the PDF content from the response
-                    pdf_file = io.BytesIO(response.content)
-                    
-                    # Define a PyPDF2 reader
-                    reader = PyPDF2.PdfReader(pdf_file)
+                    # Parse the html from the arxiv page
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.content, 'html.parser')
 
-                    # Extract the text
-                    text = ""
-                    for page in reader.pages:
-                        text += page.extract_text() + "\n"  # Adding a newline for separation
+                    # Find the <a> tag with the specific class and id
+                    html_class = soup.find('a', class_='abs-button', id='latexml-download-link')
 
-                    # Save the text to the output dict
-                    pdf_texts[paper] = text
+                    # Extract the link from the href attribute
+                    html_link = html_class.get('href')
+
+                    html_links[paper] = html_link
+
                 else:
-                    print(f"Failed to download PDF. Status code: {response.status_code}")
+                    print(f"Failed to access arxiv page. Status code: {response.status_code}")
 
             except Exception as e:
                 print(f"An error occurred: {e}")
 
-        return pdf_texts
+        return html_links
+
 
